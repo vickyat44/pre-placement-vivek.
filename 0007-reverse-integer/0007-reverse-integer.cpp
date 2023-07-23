c62efddf105fb9bf7a364f70b0dd67da13fca275
vickyat44 <77204463+vickyat44@users.noreply.github.com>
2023-07-23 21:17:45 +0530

    Time: 7 ms (17.88%), Space: 5.9 MB (89.72%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int sum=0;
+        while(x!=0)
+        {
+            if(sum>INT_MAX/10||sum<INT_MIN/10)
+            return 0;
+            int y=x%10;
+            sum=(sum*10)+y;
+            x=x/10;
+        }
+        return sum;
+    }
+};
