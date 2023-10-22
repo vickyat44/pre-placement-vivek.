a158920de169e17cead6e88e7578b2b4b1c3a526
vickyat44 <77204463+vickyat44@users.noreply.github.com>
2023-10-22 11:27:01 +0530

    Time: 8 ms (87.54%), Space: 13.9 MB (17.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        // Create two arrays to store information about which rows and columns to zero.
-        vector<bool> zeroRow(n, false);
-        vector<bool> zeroCol(m, false);
-        
-        // Iterate through the matrix and mark rows and columns that need to be zeroed.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRow[i] = true;
-                    zeroCol[j] = true;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int> rows(n, 0);
+        vector<int> cols(m, 0);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    rows[i]=1;
+                    cols[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(rows[i]==1 || cols[j]==1)
+                {
+                    matrix[i][j]=0;
                 }
             }
         }
         
-        // Update the matrix based on the information stored in the arrays.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (zeroRow[i] || zeroCol[j]) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
     }
 };
