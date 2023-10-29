8fb0c19781197c1f70a92911b5e48d1b9b37c71b
vickyat44 <77204463+vickyat44@users.noreply.github.com>
2023-10-29 23:31:57 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -16,15 +16,11 @@ class Solution{
         for(int i=0;i<n;i++)
         {
             sum+=arr[i];
-            maxi=max(sum,maxi);
+            maxi=max(maxi,sum);
             if(sum<0)
-            {
-                sum=0;
-            }
-            
+            sum=0;
         }
         return maxi;
-        
     }
 };
 
