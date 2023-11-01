463468d22e465b877a42ed5ca896e7fabc544020
vickyat44 <77204463+vickyat44@users.noreply.github.com>
2023-11-01 09:59:02 +0530

    Added solution - LeetHub

---
+++ Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+    Node * curr = head;
+    
+    while(curr!=nullptr)
+    {
+        if(curr->next!=nullptr && curr->data == curr->next->data)
+        {
+            Node * abc=curr->next->next;
+            Node * todelete = curr->next;
+            curr->next=abc;
+            delete(todelete);
+            
+        }
+        else
+        {
+            curr=curr->next;
+        }
+    }
+    return head;
+ 
+}
+
+// Node* removeDuplicates(Node* head) {
+//     Node* curr = head;
+
+//     while (curr != nullptr) {
+//         if (curr->next != nullptr && curr->data == curr->next->data) {
+//             Node* todelete = curr->next;
+//             curr->next = curr->next->next;
+//             delete todelete;
+//         } else {
+//             curr = curr->next;
+//         }
+//     }
+
+//     return head;
+// }
